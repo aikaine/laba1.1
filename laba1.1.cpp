34d26397347571bfd0b11fea93b1b4935c861bbc
ПК <@DESKTOP-9B4BKUK>
2024-02-11 19:21:29 +0300

    Добавьте файлы проекта.

---
+++ laba1.1.cpp
@@ -0,0 +1,35 @@
+﻿// laba1.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+#include <math.h>
+int main()
+{
+    double x, y, z;
+    setlocale(LC_ALL, "RU");
+    cout << "Введите x: ";
+    cin >> x;
+    if (cin.fail())
+    {
+        cout << "Введите число" << endl;
+        cin.clear();
+        while (cin.get() != '\n');
+        return 1;
+    }
+
+    cout << "Введите y: ";
+    cin >> y;
+    if (cin.fail())
+    {
+        cout << "Введите число" << endl;
+        cin.clear();
+        while (cin.get() != '\n');
+        return 1;
+    }
+
+
+    z = x * sin(y) + sqrt(abs(x * y));
+    cout << "Ответ: " << z;
+    return 0;
+}
